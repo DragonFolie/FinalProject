016a66e19f531da85f467933194de8aad6b7af38
Den <blagday2@gmail.com>
2021-08-31 01:31:00 +0300

    First

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/hello")
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void init() {
+        message = "Hello World!";
+        System.out.println("Init");
+    }
+
+
+
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)throws  IOException {
+
+        response.setContentType("text/html");
+
+
+        try (PrintWriter writer = response.getWriter()) {
+            String name = request.getParameter("username");
+            String age = request.getParameter("user_age");
+            String gender = request.getParameter("gender");
+            String country = request.getParameter("country");
+            String[] courses = request.getParameterValues("courses");
+            writer.println("<p>Name: " + name + "</p>");
+            System.out.println(name);
+            writer.println("<p>Age: " + age + "</p>");
+            writer.println("<p>Gender: " + gender + "</p>");
+            writer.println("<p>Country: " + country + "</p>");
+            writer.println("<h4>Courses</h4>");
+            for (String course : courses)
+                writer.println("<li>" + course + "</li>");
+        }
+
+
+    }
+
+    public void destroy() {
+
+        System.out.println("Destroy");
+    }
+}
---
+++ src/main/webapp/views/list.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: User
+  Date: 031 31.08.21
+  Time: 0:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<h2>List JSP</h2>
+</body>
+</html>
